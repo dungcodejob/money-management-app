297c01795139f816bc4dc77251abc6ac3e165aab
dungcodejob <dungcodejob@gmail.com>
2022-06-14 23:52:37 +0700

    vendor: setting angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'money-management-app-e315b',
+    appId: '1:776608465881:web:72066d542358324a1e8fc8',
+    storageBucket: 'money-management-app-e315b.appspot.com',
+    apiKey: 'AIzaSyDK7lOeBledJYdCOIvw9q9nI1GdyVdcb7k',
+    authDomain: 'money-management-app-e315b.firebaseapp.com',
+    messagingSenderId: '776608465881',
+    measurementId: 'G-ZZ0618GZ55',
+  },
   production: false
 };
 
